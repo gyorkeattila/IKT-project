4d24400cf31b5f1b5afb3b0f540c25999534cc11
gyorkeattila <71698318+gyorkeattila@users.noreply.github.com>
2021-10-12 14:56:33 +0200

    Update projektbot.py

--- projektbot.py
+++ projektbot.py
@@ -5,6 +5,11 @@ from discord.ext.commands import has_permissions, MissingPermissions
 
 import datetime
 
+import ffmpeg
+from discord import FFmpegPCMAudio
+
+from discord import voice_client
+from discord.utils import get
 
 
 intents = discord.Intents.all() 
@@ -17,7 +22,27 @@ async def on_ready():
   global indulas
   print('Fut a bot')
   
+#ping
+@client.command()
+async def ping(ctx):
+        embedVar = discord.Embed(title="Bot válaszideje")
+        embedVar.add_field(name="Válaszidő", value=f'{round(client.latency * 1000)}ms', inline=False)
+        await ctx.channel.send(embed=embedVar)
+  
+  
+#zene
+@client.command()
+async def zene(ctx):
+    user = ctx.author.voice
+    if user is not None:
+        channel = ctx.author.voice.channel
+        voice = await channel.connect()
+        voice.play(discord.FFmpegPCMAudio(executable="C:/Users/ati42/Desktop/ffmpeg-2021-09-01-git-c500dc7cca-full_build/bin/ffmpeg.exe", source=r"C:\Users\ati42\Desktop\Wenseron botja\friday.mp3"))
+    else:
+        await ctx.send("Nem vagy bent egy hangcsatornában sem")
+
  
+
 
 
 
